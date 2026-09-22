c8587a65deeff63ced98fa5a815924ef9b3920ec
Boymel <boymeash29@7hills.org>
2026-09-22 10:34:01 -0400

    Added !

--- Hello.java
+++ Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 	
 		public static void main(String[] args) {
 			System.out.println("Hello, World!");
-			System.out.println("It is another great day");
+			System.out.println("It is another great day!");
 		}
 		
 }
